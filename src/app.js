eb59f03898a496f24b6c513660374b2d85b4012e
Giancarlo Zuliani <slaigox@gmail.com>
2021-02-17 12:21:55 +0100

    typing animation added

--- src/app.js
+++ src/app.js
@@ -11,5 +11,77 @@ $(document).ready(function() {
         }
     });
 });
-
-console.log('ciao');
+var ghpages = require('gh-pages');
+/* ghpages.publish('dist', function(err) {}); */
+/* $("#typedbanner").typing({
+    
+    
+    
+    // time before backspacing in milliseconds
+    
+    eraseDelay: 10,
+    
+    
+    
+    // time before typing in milliseconds
+    
+    typeDelay: 70,
+    
+    
+    
+    // time before string typing in milliseconds
+    
+    stringStartDelay: 1000,
+    
+    
+    
+    // text color
+    
+    color: 'red',
+    
+    
+    
+    // text color when typing
+    
+    typingColor: 'black',
+    
+    
+    
+    // opacity when typing
+    
+    typingOpacity: '0.1',
+    
+    
+    
+    // amount of loops
+    
+    loopCount: 3,
+    
+    
+    
+    // shows blink cursor
+    
+    cursorBlink: true,
+    
+    
+    
+    // character for cursor
+    
+    cursorChar: '<small></small>',
+    
+    
+    
+    // enables fade animation
+    
+    fade: true
+    
+    
+    
+}); */
+$('#typedbanner').typing({
+    strings: ['javascript berserker', 'code hard play hard', 'full stack web developer'],
+    cursorBlink: true,
+    loopCount: 5,
+    typingColor: "#e73635",
+    color: "#e73635"
+});
