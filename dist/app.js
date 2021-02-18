ed01749e7ee9e92e1a76b6dc0779f49e44963a7d
Giancarlo Zuliani <slaigox@gmail.com>
2021-02-18 22:42:56 +0100

    contacts page complete

--- dist/app.js
+++ dist/app.js
@@ -11,10 +11,16 @@ $(document).ready(function () {
   var contentswitch = true;
   $('#nav-icon').click(function () {
     contentswitch === true ? $('section').hide() : $('section').slideDown('slow');
+    contentswitch === true ? $('#contacts').animate({
+      'left': '-100%'
+    }) : $('#contacts').animate({
+      'left': '0'
+    });
     contentswitch = !contentswitch;
     $(this).toggleClass('open');
     $('#portrait').toggleClass('slidedown');
     $('aside').toggleClass('slideleft');
+    $('#contacts').toggleClass;
     var anchors = $('aside a');
 
     var _loop = function _loop(i) {
@@ -27,86 +33,6 @@ $(document).ready(function () {
       _loop(i);
     }
   });
-});
-/* $("#typedbanner").typing({
-    
-    
-    
-    // time before backspacing in milliseconds
-    
-    eraseDelay: 10,
-    
-    
-    
-    // time before typing in milliseconds
-    
-    typeDelay: 70,
-    
-    
-    
-    // time before string typing in milliseconds
-    
-    stringStartDelay: 1000,
-    
-    
-    
-    // text color
-    
-    color: 'red',
-    
-    
-    
-    // text color when typing
-    
-    typingColor: 'black',
-    
-    
-    
-    // opacity when typing
-    
-    typingOpacity: '0.1',
-    
-    
-    
-    // amount of loops
-    
-    loopCount: 3,
-    
-    
-    
-    // shows blink cursor
-    
-    cursorBlink: true,
-    
-    
-    
-    // character for cursor
-    
-    cursorChar: '<small></small>',
-    
-    
-    
-    // enables fade animation
-    
-    fade: true
-    
-    
-    
-}); */
-
-$('#typedbanner').typing({
-  strings: ['javascript berserker', 'code hard , play hard', 'laravel enthusiast', 'jquery devoted', 'full stack web developer'],
-  cursorBlink: true,
-  loopCount: 5,
-  typingColor: "#e73635",
-  color: "#e73635"
-});
-$('#codetext').typing({
-  strings: ['LARAVEL', 'PHP', 'MySql', 'JQuery', 'JAVASCRIPT', 'SaSS'],
-  cursorBlink: true,
-  loopCount: 50,
-  typingColor: 'white',
-  color: 'white'
 });
 
 /***/ }),
